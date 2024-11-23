bcac89ef615a1fa1f4c47180e5b5dbab04dc2b2c
Tony Wang <twang35@users.noreply.github.com>
2024-11-23 08:34:11 -0500

    add appendFront test and refactor to pass in a method into runTest

--- ComparisonRunner.java
+++ ComparisonRunner.java
@@ -1,25 +1,42 @@
 import java.util.ArrayList;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 public class ComparisonRunner {
-    public static void main(String[] args) {
-        System.out.println("comparison runner");
+    Map<String, Integer> nMappings = Map.of(
+        "forLoops", 1_000_000_000,
+        "appendFront", 100_000
+    );
 
-        int repeats = 10;
-        ArrayList<Long> times = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
+        System.out.println("comparison runner");
 
         ComparisonRunner runner = new ComparisonRunner();
 
+        Method testMethod = ComparisonRunner.class.getDeclaredMethod("appendFront", long.class);
+
+        runner.runTest(testMethod);
+    }
+
+    private void runTest(Method method) throws Exception{
+        int repeats = 10;
+        ArrayList<Long> times = new ArrayList<>();
+        Object[] parameters = new Object[1];
+        int n = this.nMappings.get(method.getName());
+
         for (int i = 0; i < repeats; i++) {
+            parameters[0] = n + i;
             long start = System.currentTimeMillis();
-            long sum = runner.forLoops(1_000_000_000 + i);
+            long sum = (long) method.invoke(this, parameters);
             long end = System.currentTimeMillis();
             System.out.println(sum);
             System.out.println(end-start);
             times.add(end-start);
         }
-        System.out.printf("forLoops: %s\n", times);
+        System.out.printf("%s: %s (ms)\n", method.getName(), times);
     }
 
+    @SuppressWarnings("unused")
     private long forLoops(long n) {
         long sum = 0;
 
@@ -28,5 +45,15 @@ public class ComparisonRunner {
         }
 
         return sum;
+    }
+
+    @SuppressWarnings("unused")
+    private long appendFront(long n) {
+        ArrayList<Long> testList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            testList.addFirst(n);
+        }
+
+        return testList.getLast();
     }
 }
