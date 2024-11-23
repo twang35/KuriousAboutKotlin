bfeaead31f85c3bfcecabde82ac894c6136c2866
Tony Wang <twang35@users.noreply.github.com>
2024-11-23 08:38:58 -0500

    add appendLast method

--- ComparisonRunner.java
+++ ComparisonRunner.java
@@ -5,7 +5,8 @@ import java.util.Map;
 public class ComparisonRunner {
     Map<String, Integer> nMappings = Map.of(
         "forLoops", 1_000_000_000,
-        "appendFront", 100_000
+        "appendFirst", 100_000,
+        "appendLast", 20_000_000
     );
 
     public static void main(String[] args) throws Exception {
@@ -13,7 +14,7 @@ public class ComparisonRunner {
 
         ComparisonRunner runner = new ComparisonRunner();
 
-        Method testMethod = ComparisonRunner.class.getDeclaredMethod("appendFront", long.class);
+        Method testMethod = ComparisonRunner.class.getDeclaredMethod("appendLast", long.class);
 
         runner.runTest(testMethod);
     }
@@ -48,10 +49,20 @@ public class ComparisonRunner {
     }
 
     @SuppressWarnings("unused")
-    private long appendFront(long n) {
+    private long appendFirst(long n) {
         ArrayList<Long> testList = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             testList.addFirst(n);
+        }
+
+        return testList.getLast();
+    }
+
+    @SuppressWarnings("unused")
+    private long appendLast(long n) {
+        ArrayList<Long> testList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            testList.addLast(n);
         }
 
         return testList.getLast();
